3a14d4e16ab1a02953b662f54a098085b170d5c9
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-06 23:37:35 +0100

    chore: setup prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       aspectRatio: {
-        '4/3': '4 / 3',
-        '3/2': '3 / 2',
+        "4/3": "4 / 3",
+        "3/2": "3 / 2",
       },
     },
   },
