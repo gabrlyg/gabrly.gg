6934aa05fbe2b5b1669a03c8f298ee157ec24704
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-15 00:35:30 +0100

    feat: update styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import BurgerMenu from './BurgerMenu/BurgerMenu';
 import Navigation from './Navigation';
 
 const Header: React.FC = () => {
   return (
     <header className="w-full sticky left-0 top-0 right-0 flex flex-row justify-center bg-slate-100">
-      {/* <BurgerMenu /> */}
       <Navigation />
     </header>
   );
--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -24,14 +24,16 @@ interface ToggleProps {
 const Logo: React.FC<LogoProps> = ({ darkMode }) => {
   return (
     <h1 className="w-10 h-10">
-      <img src="/assets/logo.svg" />
+      <a href="/">
+        <img src="/assets/logo.svg" />
+      </a>
     </h1>
   );
 };
 
 const LinksList: React.FC = () => {
   return (
-    <ul className="flex flex-col md:flex-row gap-4 text-lg">
+    <ul className="flex flex-col items-end md:flex-row gap-4 md:text-lg">
       <li>
         <a href="/keyboard">Keyboard</a>
       </li>
@@ -44,15 +46,15 @@ const LinksList: React.FC = () => {
 
 const NavButtonIcon: React.FC<{ expanded: boolean }> = ({ expanded }) => {
   return (
-    <div className="flex flex-col justify-between items-end h-[21px] w-5 overflow-hidden">
+    <div className="flex flex-col justify-between items-end h-[20px] w-5 overflow-hidden">
       <div
-        className={`bg-black h-[1px] w-7 transform transition-all origin-right duration-200 ${expanded ? 'rotate-[-44.25deg]' : ''}`}
+        className={`bg-black h-[2px] w-7 transform transition-all origin-right duration-200 ${expanded ? 'rotate-[-41deg]' : ''}`}
       />
       <div
-        className={`bg-black h-[1px] w-1/2 transform transition-all origin-right duration-200 ${expanded ? 'translate-x-10' : ''}`}
+        className={`bg-black h-[2px] w-1/2 transform transition-all origin-right duration-200 ${expanded ? 'translate-x-10' : ''}`}
       />
       <div
-        className={`bg-black h-[1px] w-7 transform transition-all origin-right duration-200 ${expanded ? 'rotate-[44.25deg]' : ''}`}
+        className={`bg-black h-[2px] w-7 transform transition-all origin-right duration-200 ${expanded ? 'rotate-[41deg]' : ''}`}
       />
     </div>
   );
@@ -61,7 +63,7 @@ const NavButtonIcon: React.FC<{ expanded: boolean }> = ({ expanded }) => {
 const NavButton: React.FC<NavButtonProps> = ({ onPress, isExpanded }) => {
   return (
     <button
-      className="inline-flex items-center justify-center w-9 h-9 rounded-md active:bg-gray-200 z-[999]"
+      className="flex items-center justify-center w-9 h-9 rounded-sm active:bg-slate-200 z-[999]"
       onClick={onPress}
     >
       <NavButtonIcon expanded={isExpanded} />
@@ -82,7 +84,7 @@ const Toggle: React.FC<ToggleProps> = ({ label, isOn, toggle }) => {
         onChange={toggle}
       />
       <span
-        className={`h-8 w-16 rounded-sm border-[1px] relative border-slate-700 before:toggle-slider peer-checked/checkbox:before:toggle-slider-on`}
+        className={`h-8 w-16 rounded-sm border-[2px] relative border-slate-700 before:toggle-slider peer-checked/checkbox:before:toggle-slider-on`}
         role="checkbox"
         onClick={toggle}
       />
@@ -106,12 +108,15 @@ const NavigationMenu: React.FC<NavigationMenuProps> = ({ isExpanded }) => {
 
   return (
     <div
-      className={`absolute right-2 top-6 border-[1px] bg-slate-100 pt-16 px-4 pb-4 flex items-end flex-col rounded-sm ${isExpanded ? 'border-slate-700 shadow-box-md' : 'hidden'}`}
+      className={`absolute right-2 top-6 border-[2px] bg-slate-100 pt-16 px-4 pb-4 flex items-end flex-col gap-4 rounded-sm ${isExpanded ? 'border-slate-700 shadow-box-md' : 'hidden'}`}
       aria-hidden={!isExpanded}
     >
       <div className="flex md:hidden">
         <LinksList />
       </div>
+      {/* Beginning of separator between link list and toggles */}
+      <div className="h-[1px] w-full bg-slate-600 md:hidden" />
+      {/* Ending of separator between link list and toggles */}
       <ul className={`flex flex-col w-full items-end gap-4`}>
         {/* <li>
           <Toggle label="Dark Mode" isOn={false} toggle={() => {}} />
@@ -148,9 +153,9 @@ const Navigation: React.FC = () => {
   }, [isMounted]);
 
   return (
-    <nav className="w-full flex flex-row justify-between items-center h-24 p-4">
+    <nav className="w-full flex items-center h-24 max-w-2xl p-4 relative">
       <Logo />
-      <div className="flex flex-row items-center gap-4">
+      <div className="flex flex-row items-center gap-4 flex-grow justify-end">
         <div className="hidden md:inline-flex">
           <LinksList />
         </div>
