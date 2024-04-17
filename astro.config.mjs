0da1737fb267f7c99806f357fc7a313a33b65996
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-18 00:33:26 +0100

    chore: setup astro db

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-
 import netlify from '@astrojs/netlify';
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx()],
+  integrations: [react(), tailwind(), mdx(), db()],
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify()
 });
