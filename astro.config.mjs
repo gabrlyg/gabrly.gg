806719fd23fc9435cdad59b2c22af1cc99596363
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-18 01:22:53 +0100

    Revert "chore: setup astro db"
    This reverts commit 04fca2e7cf559e4330c2bf9cd20c50ae34bffffe.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify';
 
-import db from "@astrojs/db";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), db()],
+  integrations: [react(), tailwind(), mdx()],
   output: 'server',
-  adapter: netlify()
+  adapter: netlify(),
 });
