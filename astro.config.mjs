cc56e30eac8309d709a076697ad43318d2840b38
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-18 01:35:01 +0100

    chore: cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,6 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), mdx()],
