ff796be4d9c246aaa7876a70bab764cfed0f267b
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-20 00:32:37 +0100

    feat: full screen gallery navigation buttons

--- src/components/KeyboardGallery/Album.tsx
+++ src/components/KeyboardGallery/Album.tsx
@@ -64,22 +64,48 @@ const FullScreenMode: React.FC<FullScreenModeProps> = ({
 }) => {
   const [currentImage, setCurrentImage] = useState<number>(coverIndex);
 
+  const previousImage = useCallback(() => {
+    setCurrentImage(currentImage === 0 ? images.length - 1 : currentImage - 1);
+  }, [currentImage, setCurrentImage]);
+
+  const nextImage = useCallback(() => {
+    setCurrentImage(currentImage === images.length - 1 ? 0 : currentImage + 1);
+  }, [currentImage, setCurrentImage]);
+
   return (
     <div className="fixed inset-0 z-50 bg-black flex flex-col justify-between items-center gap-4 p-4">
+      {/* begin close button */}
       <button
-        className="flex justify-center flex-shrink-0 items-center w-10 h-10 bg-slate-200 text-slate-700 rounded-full self-end mt-4"
+        className="flex justify-center flex-shrink-0 items-center w-10 h-10 text-slate-200 text-2xl self-end mt-4"
         onClick={handleClose}
+        aria-label="Close"
       >
-        X
+        ✕
       </button>
-      {/* `overflow-hidden` here is necessary other wise image will try to take
+      {/* end close button */}
+      {/* begin main image
+      `overflow-hidden` here is necessary other wise image will try to take
       100% of the width */}
-      <div className="flex justify-center items-center overflow-hidden">
-        <img
-          className="max-h-full object-contain flex-shrink"
-          src={`${PHOTO_PATH_PREFIX}${images[currentImage]}`}
-        />
+      <div className="flex flex-row justify-center items-center overflow-hidden relative w-full">
+        <button
+          className="w-10 h-10 text-2xl text-slate-200 flex-shrink-0"
+          onClick={previousImage}
+          aria-label="Previous Image"
+        >{`<`}</button>
+        <div className="w-full h-full">
+          <img
+            className="w-full h-full object-contain"
+            src={`${PHOTO_PATH_PREFIX}${images[currentImage]}`}
+          />
+        </div>
+        <button
+          className="w-10 h-10 text-2xl text-slate-200 flex-shrink-0"
+          onClick={nextImage}
+          aria-label="Next Image"
+        >{`>`}</button>
       </div>
+      {/* end main image */}
+      {/* begin thumbnail */}
       <div className="flex justify-center items-center">
         <ul className="flex flex-row flex-shrink-0 overflow-x-auto overflow-y-hidden gap-4 h-40 w-full scroll-smooth snap-x items-center">
           {images.map((image, index) => (
@@ -92,6 +118,7 @@ const FullScreenMode: React.FC<FullScreenModeProps> = ({
           ))}
         </ul>
       </div>
+      {/* end thumbnail */}
     </div>
   );
 };
