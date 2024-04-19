e93cbe87ca14d47ab974beace3f58fa3132cf18d
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-19 01:41:35 +0100

    feat: implemented thumbnail snapping

--- src/components/KeyboardGallery/Album.tsx
+++ src/components/KeyboardGallery/Album.tsx
@@ -1,15 +1,103 @@
-import React, { useCallback, useId, useState } from 'react';
+import React, { useCallback, useEffect, useId, useRef, useState } from 'react';
 import type { Keyboard } from '../../pages/keyboard-gallery/fakeDB';
+
+interface ThumbnailProps {
+  image: string;
+  index: number;
+  selected: boolean;
+  setCurrentImage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+interface FullScreenModeProps {
+  images: string[];
+  coverIndex: number;
+  handleClose: React.MouseEventHandler<HTMLButtonElement>;
+}
 
 const PHOTO_PATH_PREFIX =
   'https://s3.eu-west-2.amazonaws.com/gabrly.gg/gallery/keyboards/';
 
-const Album: React.FC<Keyboard> = ({ name, images, cover }) => {
-  const id = useId();
-  const [isFullScreenOn, setIsFullScreenOn] = useState<boolean>(false);
-  const [currentImage, setCurrentImage] = useState<number>(
-    images.findIndex((image) => image === cover)
+const ImageThumbnail: React.FC<ThumbnailProps> = ({
+  image,
+  index,
+  selected,
+  setCurrentImage,
+}) => {
+  const ref = useRef<HTMLLIElement>(null);
+  const [isFirstRender, setIsFirstRender] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (selected) {
+      ref.current?.scrollIntoView({
+        behavior: isFirstRender ? 'instant' : 'smooth',
+        block: 'center',
+        inline: 'center',
+      });
+    }
+  }, [selected, isFirstRender]);
+
+  useEffect(() => {
+    setIsFirstRender(false);
+  }, []);
+
+  return (
+    <li ref={ref} key={index} className="snap-center">
+      <button
+        onClick={() => {
+          setCurrentImage(index);
+        }}
+      >
+        <img
+          className="max-h-44 min-w-40 object-contain"
+          src={`${PHOTO_PATH_PREFIX}${image}`}
+          draggable={false}
+        />
+      </button>
+    </li>
   );
+};
+
+const FullScreenMode: React.FC<FullScreenModeProps> = ({
+  images,
+  coverIndex,
+  handleClose,
+}) => {
+  const [currentImage, setCurrentImage] = useState<number>(coverIndex);
+
+  return (
+    <div className="fixed inset-0 bg-black flex flex-col justify-between gap-4 p-4">
+      <button
+        className="flex justify-center flex-shrink-0 items-center w-10 h-10 bg-slate-200 text-slate-700 rounded-full self-end"
+        onClick={handleClose}
+      >
+        X
+      </button>
+      {/* `overflow-hidden` here is necessary other wise image will try to take
+      100% of the width */}
+      <div className="flex justify-center items-center max-h-full overflow-hidden">
+        <img
+          className="max-h-full max-w-full object-contain"
+          src={`${PHOTO_PATH_PREFIX}${images[currentImage]}`}
+        />
+      </div>
+      <ul className="flex flex-row overflow-x-scroll gap-4 min-h-44 scroll-smooth snap-x items-center">
+        {images.map((image, index) => (
+          <ImageThumbnail
+            image={image}
+            index={index}
+            selected={index === currentImage}
+            setCurrentImage={setCurrentImage}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Album: React.FC<Keyboard> = ({ name, images, cover }) => {
+  const coverImageIndex = images.findIndex((image) => image === cover);
+
+  const [isFullScreenOn, setIsFullScreenOn] = useState<boolean>(false);
 
   const openFullScreen: React.MouseEventHandler<HTMLDivElement> = useCallback(
     (e) => {
@@ -39,42 +127,11 @@ const Album: React.FC<Keyboard> = ({ name, images, cover }) => {
         <label className="text-sm">{name}</label>
       </div>
       {isFullScreenOn && (
-        <div className="fixed inset-0 bg-black flex flex-col justify-between gap-4 p-4">
-          <button
-            className="flex justify-center flex-shrink-0 items-center w-10 h-10 bg-slate-200 text-slate-700 rounded-full self-end"
-            onClick={handleFullScreenClose}
-          >
-            X
-          </button>
-          {/* `overflow-hidden` here is necessary other wise image will try to take
-          100% of the width */}
-          <div className="flex justify-center items-center max-h-full overflow-hidden">
-            <img
-              className="max-h-full max-w-full object-contain"
-              src={`${PHOTO_PATH_PREFIX}${images[currentImage]}`}
-            />
-          </div>
-          <ul className="flex flex-row overflow-x-scroll gap-4 min-h-44 scroll-smooth">
-            {images.map((image, index) => {
-              return (
-                <li key={index}>
-                  <button
-                    className=""
-                    onClick={() => {
-                      setCurrentImage(index);
-                    }}
-                  >
-                    <img
-                      className="max-h-44 min-w-40 object-contain"
-                      src={`${PHOTO_PATH_PREFIX}${image}`}
-                      draggable={false}
-                    />
-                  </button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+        <FullScreenMode
+          images={images}
+          coverIndex={coverImageIndex}
+          handleClose={handleFullScreenClose}
+        />
       )}
     </>
   );
