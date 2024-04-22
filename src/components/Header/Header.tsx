9af1f585d52cf27721e7dc3f4968df776de1e4c7
gabrlyg <gang.gabriel.yang@gmail.com>
2024-04-22 02:01:30 +0100

    fix: fix z-index, again

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,7 @@ interface Props {
 const Header: React.FC<Props> = ({ isHomePage }) => {
   return (
     <header
-      className={`w-full ${isHomePage ? '' : 'sticky'} left-0 top-0 right-0 flex flex-row justify-center bg-slate-100 z-[999]`}
+      className={`w-full ${isHomePage ? '' : 'sticky'} left-0 top-0 right-0 flex flex-row justify-center bg-slate-100 z-[998]`}
     >
       <Navigation isHomePage={isHomePage} />
     </header>
